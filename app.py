f99749caccad1518fb4791298d631fa72bae252e
madhansubbiah <madhan.subbiah@gmail.com>
2025-04-02 08:50:05 -0400

    further version

--- app.py
+++ app.py
@@ -12,8 +12,9 @@ from langchain_chroma import Chroma
 from langchain_core.documents import Document
 import urllib3
 from dotenv import load_dotenv
-import(‘pysqlite3’) 
-import sys sys.modules[‘sqlite3’] = sys.modules.pop(‘pysqlite3’)
+import(pysqlite3) 
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 
 st.write("The path ",os.path.dirname(sys.executable))
 
